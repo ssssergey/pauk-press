a3e0ffa155adc726d3074b66af64ec709a3ecf0c
Sergey <lse1983@mail.ru>
2016-02-21 14:35:20 +0300

    Added simple API

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, session, url_for, request, g, send_file
+from flask import render_template, flash, redirect, session, url_for, request, g, send_file, jsonify
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm
 from .forms import LoginForm, RegistrationForm, FeedbackForm
@@ -40,9 +40,9 @@ rss_dict = {u'Би-Би-Си': 'http://www.bbc.co.uk/russian/',
 			u'САНА': 'http://sana.sy/ru/',
 			u'РИА Новости': 'http://ria.ru/',
 			u'Грузия-онлайн': 'http://apsny.ge/',
-			u'ДАН':'http://dan-news.info/',
-			u'Анадолу':'http://aa.com.tr/ru/',
-			u'Арменпресс':'http://armenpress.am/rus/'
+			u'ДАН': 'http://dan-news.info/',
+			u'Анадолу': 'http://aa.com.tr/ru/',
+			u'Арменпресс': 'http://armenpress.am/rus/'
 			}
 
 
@@ -52,7 +52,7 @@ def index():
 	query = Main.query.order_by("article_time desc").limit(100).all()
 	posts = []
 	for row in query:
-		new_row = [row.article_title, ConvertTimeFormat(row.article_time),row.rss_source,row.id]
+		new_row = [row.article_title, ConvertTimeFormat(row.article_time), row.rss_source, row.id]
 		posts.append(new_row)
 	return render_template("main.html", NEWS=posts)
 
@@ -60,7 +60,8 @@ def index():
 def main_edit():
 	return render_template("main_edit.html")
 
-@app.route('/register', methods=["GET","POST"])
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
 	form = RegistrationForm(request.form)
 
@@ -71,7 +72,7 @@ def register():
 	print form.errors
 
 	if request.method == "POST" and form.validate():
-		nickname  = form.username.data
+		nickname = form.username.data
 		email = form.email.data
 		password = sha256_crypt.encrypt((str(form.password.data)))
 
@@ -104,7 +105,7 @@ def login():
 	print form.errors
 
 	if request.method == "POST" and form.validate_on_submit():
-		nickname  = form.username.data
+		nickname = form.username.data
 		password = str(form.password.data)
 
 		user = User.query.filter_by(nickname=nickname).first()
@@ -115,7 +116,7 @@ def login():
 			if 'remember_me' in session:
 				remember_me = session['remember_me']
 				session.pop('remember_me', None)
-			login_user(user, remember = remember_me)
+			login_user(user, remember=remember_me)
 			flash(u"Поздравляю, {}, Вы успешно вошли.".format(user.nickname))
 			return redirect(request.args.get('next') or url_for('index'))
 		else:
@@ -127,18 +128,22 @@ def login():
 						   form=form
 						   )
 
+
 @app.before_request
 def before_request():
 	g.user = current_user
+
 
 @app.route('/logout')
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
+
 @lm.user_loader
 def load_user(id):
 	return User.query.get(int(id))
+
 
 @app.route('/user/<nickname>')
 @login_required
@@ -155,6 +160,7 @@ def user(nickname):
 						   user=user,
 						   posts=posts)
 
+
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
 	form = FeedbackForm(request.form)
@@ -166,14 +172,15 @@ def feedback():
 	print form.errors
 
 	if request.method == "POST" and form.validate():
-		nickname  = form.username.data
+		nickname = form.username.data
 		message = form.message.data
 		import os
+
 		basedir = os.path.abspath(os.path.dirname(__file__))
 		feedback_path = os.path.join(basedir, 'feedback.txt')
 		with open(feedback_path, 'a') as history_txt:
 			history_txt.write('{} | {}\n{}\n\n'.format(datetime.datetime.utcnow(),
-                                                       nickname.encode('utf8'),message.encode('utf8')))
+													   nickname.encode('utf8'), message.encode('utf8')))
 		feedback_notification(nickname, message)
 		flash(u"Ваше сообщение получено!")
 		return redirect(request.args.get('next') or url_for('index'))
@@ -182,48 +189,88 @@ def feedback():
 	return render_template("feedback.html", form=form)
 
 
+# JSON API Search
+@app.route('/api_search/<word>', methods=['GET'])
+def api_search(word):
+	bunch = []
+	bunch.append((("%" + word.lower() + "%"), ("%" + word.capitalize() + "%"), ("%" + word.upper() + "%")))
+	args = []
+	for pair in bunch:
+		args.append(or_(Main.article_title.like(pair[0]),
+						Main.article_title.like(pair[1]),
+						Main.article_title.like(pair[2]),
+						Main.article_text.like(pair[0]),
+						Main.article_text.like(pair[1]),
+						Main.article_text.like(pair[2]),
+						))
+	query = Main.query.filter(and_(*[i for i in args])).order_by("article_time desc").limit(1000).all()
+	posts = []
+	for row in query:
+		new_row = [row.article_time, row.article_title, row.rss_source, row.id]
+		posts.append(new_row)
+	return json.dumps({'items':posts}, ensure_ascii=False)
+
+import json
+@app.route('/api_search/item/<int:id>', methods=['GET'])
+def api_item(id):
+	query_row = Main.query.filter(Main.id == int(id)).first()
+	if query_row:
+		posts = {'time_api':query_row.article_time,
+				 'title_api':query_row.article_title,
+				 'text_api':query_row.article_text,
+				 'source_api':query_row.rss_source}
+	else:
+		posts = {'time_api':'',
+				 'title_api':'',
+				 'text_api':'',
+				 'source_api':''}
+	return json.dumps(posts, ensure_ascii=False)
+
 #########################################################################
 
-@app.route('/news_search/', methods=['GET','POST'])
+@app.route('/news_search/', methods=['GET', 'POST'])
 @login_required
 def news_search():
 	if request.method == 'POST':
-		words = request.form['word1'],request.form['word2'],request.form['word3']
+		words = request.form['word1'], request.form['word2'], request.form['word3']
 		bunch = []
 		for i in words:
 			if i:
-				bunch.append((("%" + i + "%"),("%" + i.capitalize() + "%")))
+				bunch.append((("%" + i.lower() + "%"), ("%" + i.capitalize() + "%"), ("%" + i.upper() + "%")))
 		args = []
 		for pair in bunch:
 			args.append(or_(Main.article_title.like(pair[0]),
 							Main.article_title.like(pair[1]),
+							Main.article_title.like(pair[2]),
 							Main.article_text.like(pair[0]),
 							Main.article_text.like(pair[1]),
+							Main.article_text.like(pair[2]),
 							))
-		print(pair)
-		query = Main.query.filter(and_(*[i for i in args])).order_by("article_time desc").all()
+		query = Main.query.filter(and_(*[i for i in args])).order_by("article_time desc").limit(1500).all()
 		posts = []
 		for row in query:
-			new_row = [row.article_title, ConvertTimeFormat(row.article_time),row.rss_source,row.id]
+			new_row = [row.article_title, ConvertTimeFormat(row.article_time), row.rss_source, row.id]
 			posts.append(new_row)
 		return render_template("news.html", NEWS=posts)
 	else:
 		return render_template("news_search.html")
+
 
 @app.route('/<country>')
 def find_country(country):
 	query = Main.query.filter(Main.country == country).order_by("article_time desc").limit(1500).all()
 	posts = []
 	for row in query:
-		new_row = [row.article_title, ConvertTimeFormat(row.article_time),row.rss_source,row.id]
+		new_row = [row.article_title, ConvertTimeFormat(row.article_time), row.rss_source, row.id]
 		posts.append(new_row)
 	return render_template("news.html", NEWS=posts, country=country)
+
 
 @app.route('/news_body/<id>')
 def news_body(id):
 	query_row = Main.query.filter(Main.id == int(id)).first()
 	posts = (query_row.article_title, ConvertTimeFormat(query_row.article_time), query_row.rss_source,
-			 query_row.article_text.replace('\n','<br>'))
+			 query_row.article_text.replace('\n', '<br>'))
 	url_article = query_row.article_text.split('\n')[-1]
 	if url_article[:4] == 'http':
 		source_url = url_article
@@ -237,15 +284,18 @@ def news_page_az():
 	NEWS = exercises_help_function(u"азербайдж")
 	return render_template("news.html", NEWS=NEWS)
 
+
 @app.route('/news_ge/')
 def news_page_ge():
 	NEWS = exercises_help_function(u"грузи")
 	return render_template("news.html", NEWS=NEWS)
 
+
 @app.route('/news_ua/')
 def news_page_ua():
 	NEWS = exercises_help_function(u"украин")
 	return render_template("news.html", NEWS=NEWS)
+
 
 # НАТО
 @app.route('/news_na/')
@@ -253,20 +303,24 @@ def news_page_na():
 	NEWS = exercises_help_function(u"НАТО")
 	return render_template("news.html", NEWS=NEWS)
 
+
 @app.route('/news_po/')
 def news_page_po():
-	NEWS = exercises_help_function(u"польш",u'польск')
+	NEWS = exercises_help_function(u"польш", u'польск')
 	return render_template("news.html", NEWS=NEWS)
+
 
 @app.route('/news_la/')
 def news_page_la():
-	NEWS = exercises_help_function(u"латви",u'эстон',u'литва',u'прибалти')
+	NEWS = exercises_help_function(u"латви", u'эстон', u'литва', u'прибалти')
 	return render_template("news.html", NEWS=NEWS)
+
 
 @app.route('/news_tr/')
 def news_page_tr():
-	NEWS = exercises_help_function(u"турци",u'турецк')
+	NEWS = exercises_help_function(u"турци", u'турецк')
 	return render_template("news.html", NEWS=NEWS)
+
 
 # Регионы
 @app.route('/news_bl/')
@@ -274,15 +328,18 @@ def news_page_bl():
 	NEWS = sea_help_function(u"черн")
 	return render_template("news.html", NEWS=NEWS)
 
+
 @app.route('/news_md/')
 def news_page_md():
 	NEWS = sea_help_function(u"средиземно")
 	return render_template("news.html", NEWS=NEWS)
 
+
 @app.route('/pers_gulf/')
 def news_pers_gulf():
 	NEWS = sea_help_function(u"персидск")
 	return render_template("news.html", NEWS=NEWS)
+
 
 def exercises_help_function(*args):
 	bunch = []
@@ -290,18 +347,19 @@ def exercises_help_function(*args):
 		bunch.append("%" + i + "%")
 		bunch.append("%" + i.capitalize() + "%")
 	query = Main.query.filter(or_(Main.article_title.like(u'%учени%'),
-										Main.article_title.like(u'%Учени%'),
-										Main.article_title.like(u'%отраб%'),
-										Main.article_title.like(u'%Отраб%'),
-										Main.article_title.like(u'%трениров%'),
-										Main.article_title.like(u'%Трениров%'),
-								   ))
+								  Main.article_title.like(u'%Учени%'),
+								  Main.article_title.like(u'%отраб%'),
+								  Main.article_title.like(u'%Отраб%'),
+								  Main.article_title.like(u'%трениров%'),
+								  Main.article_title.like(u'%Трениров%'),
+								  ))
 	query = query.filter(or_(*[Main.article_title.like(i) for i in bunch])).order_by("article_time desc")
 	posts = []
 	for row in query:
-		new_row = [row.article_title, ConvertTimeFormat(row.article_time),row.rss_source,row.id]
+		new_row = [row.article_title, ConvertTimeFormat(row.article_time), row.rss_source, row.id]
 		posts.append(new_row)
 	return posts
+
 
 def sea_help_function(*args):
 	bunch = []
@@ -309,40 +367,44 @@ def sea_help_function(*args):
 		bunch.append("%" + i + "%")
 		bunch.append("%" + i.capitalize() + "%")
 	query = Main.query.filter(or_(Main.article_title.like(u'%морск%'),
-									Main.article_title.like(u'%море%'),
-									Main.article_title.like(u'%акватори%'),
-									Main.article_title.like(u'%залив%'),
-							   ))
+								  Main.article_title.like(u'%море%'),
+								  Main.article_title.like(u'%акватори%'),
+								  Main.article_title.like(u'%залив%'),
+								  ))
 	query = query.filter(or_(*[Main.article_title.like(i) for i in bunch])).order_by("article_time desc")
 	posts = []
 	for row in query:
-		new_row = [row.article_title, ConvertTimeFormat(row.article_time),row.rss_source,row.id]
+		new_row = [row.article_title, ConvertTimeFormat(row.article_time), row.rss_source, row.id]
 		posts.append(new_row)
 	return posts
 
-@app.route('/download_articles/', methods=['GET','POST'])
+
+@app.route('/download_articles/', methods=['GET', 'POST'])
 def download_articles():
 	try:
 		articles_ids = request.form.getlist("selected")
 		all_selected_web = ''
 		for id in articles_ids:
 			query_row = Main.query.filter(Main.id == int(id)).first()
-			ARTICLE = (query_row.article_title,ConvertTimeFormat(query_row.article_time),
-					   query_row.rss_source,query_row.article_text)
+			ARTICLE = (query_row.article_title, ConvertTimeFormat(query_row.article_time),
+					   query_row.rss_source, query_row.article_text)
 			all_selected_web = all_selected_web + '<br>'.join(ARTICLE) + '<br><br>'
 		return all_selected_web
 	except Exception as e:
 		return (str(e))
 
+
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template("404.html")
 
+
 # GET FILE
 @app.route('/get_file/')
 def get_file():
-	download_notification('Somebody','downloaded')
+	download_notification('Somebody', 'downloaded')
 	return send_file('static/download/Паук 3.5.rar', as_attachment=True)
+
 
 def ConvertTimeFormat(utc_format):
 	try:
